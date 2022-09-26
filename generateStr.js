053703e167027eb2d1ecf5d6c16df661802a0feb
Aleksander Maj <majalex@outlook.com>
2022-09-26 19:20:01 +0200

    poprawka

--- generateStr.js
+++ generateStr.js
@@ -3,10 +3,12 @@ function generateStr (num) {
     var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz0123456789';
             
     for (let i = 1; i <= num; i++) {
-        var char = Math.floor(Math.random() * str.length + 1);
+        var char = Math.floor(Math.random() * str.length);
         pass += str.charAt(char)
     }
     return pass;
 }
+
+
 
 module.exports = generateStr;
